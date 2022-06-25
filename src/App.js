21884c155141be08cccff7bc9bbb580960c3acbd
Sandeepkodhati <sandeepkodhati77@gmail.com>
2022-06-25 13:18:47 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div><h1>sdfkkfgk</h1></div>
-  );
+    <div>App</div>
+  )
 }
+
+
 
 export default App;
